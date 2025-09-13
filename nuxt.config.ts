89ade9536477e23a388adad579ff45822a5061ed
Diane <diane.de-benedetti@etudiant.univ-reims.fr>
2025-09-13 20:17:13 +0200

    feat: not indexables by robots

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2025-09-13',
   devtools: { enabled: true },
+  site: { indexable: false },
 
   modules: [
     '@nuxt/eslint',
